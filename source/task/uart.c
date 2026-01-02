8ef2a7a2c96cd1e92a46f2850b7d37b6a559cd20
huangJiazhi <huangjia2hi@163.com>
2026-01-02 08:36:04 +0800

    motor串口联动实例 串口发送数据控制电机，电机反馈当前角度

--- source/task/uart.c
+++ source/task/uart.c
@@ -2,14 +2,22 @@
 #include "cmsis_os2.h"
 #include "uart_api.h"
 #include "usart.h"
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+#include <float.h>
+#include <string.h>
 
-uart_rx_t testUart_msg;
+uart_rx_t  testUart_msg;
 uart_msg_t testUart_rx_msg;
 uart_msg_t testUart_tx_msg;
-uint8_t testUartBuffer[18];
-uint32_t lastCount;
+uart_msg_t feedbackData_msg;
+uint8_t    testUartBuffer[18];
+uint32_t   lastCount;
+
+float dm_angle = 0;
+float dm_tor = 0;
 
 extern void (*uart7_rx_hook)(uint8_t *pData, uint32_t size);
 static float dm_pos = 0;
@@ -20,11 +28,11 @@ void uart7_test_hook(uint8_t *buf, uint32_t len)
 }
 
 void testUart_rx_init(void){
-    testUart_msg.rx_msg =  &testUart_rx_msg;
-    testUart_msg.rx_msg-> pBuffer  = testUartBuffer;
-    testUart_msg.rx_msg->huart = &huart7; 
-    testUart_msg.rx_msg->Len = 18;
-    uart7_rx_hook = uart7_test_hook;
+    testUart_msg.rx_msg           =  &testUart_rx_msg;
+    testUart_msg.rx_msg-> pBuffer = testUartBuffer;
+    testUart_msg.rx_msg-> huart   = &huart7; 
+    testUart_msg.rx_msg-> Len     = 18;
+    uart7_rx_hook                 = uart7_test_hook;
     uart_rx_init(&testUart_msg);
 }
 void testUart_tx_init(void){
@@ -42,15 +50,42 @@ void uart_Refresh(void)
         lastCount = testUart_msg.count;
     }
 }
+uint8_t buf[16];
+float feedbackAngle = 0;
+void feedbackData_init(void)
+{
+    feedbackAngle = dm_angle * 10000;
+    sprintf((char *)buf, "%.2d\r\n",(int)feedbackAngle);
+    feedbackData_msg.pBuffer = (uint8_t *)buf;
+    feedbackData_msg.huart= &huart7;
+    feedbackData_msg.Len = 10;
+}
+char feedback_buf[64] = {0};
+void send_feedbackData(void)
+{
+    feedbackAngle = dm_angle * 10000;
+    int len = sprintf((char *)buf, "%.2d\r\n",(int)feedbackAngle);
+
+    feedbackData_msg.pBuffer = (uint8_t *)buf;
+    feedbackData_msg.Len     = len;
+}
 void uart_test(void *arguments)
 {
     UNUSED(arguments);
     osDelay(10);
     testUart_rx_init();
     testUart_tx_init();
+    feedbackData_init();
+    osDelay(10);
+    uart_tx_send_IT(&feedbackData_msg);
+        // send_feedbackData();
+
+    // uart_tx_send_IT(&feedbackData_msg);
     while (1) {
-        uart_tx_send_IT(&testUart_tx_msg);
+        // uart_tx_send_IT(&testUart_tx_msg);
+        send_feedbackData();
         osDelay(500);
+        uart_tx_send_IT(&feedbackData_msg);
     }
 }
 
@@ -80,9 +115,6 @@ void motor_test(void *argument){
     // 启用电机
     Motor_DM_Enable(DM_motor);
     
-    float angle = 0;
-    float tor = 0;
-
     // 无限循环
     while (true)
     {
@@ -90,10 +122,11 @@ void motor_test(void *argument){
         Motor_DM_Refresh(DM_motor);
 
         // 获取电机角度
-        angle = DM_motor->motor_msg.motor_angle;
-        tor   = DM_motor->motor_msg.torque_current;
-
+        dm_angle = DM_motor->motor_msg.motor_angle;
+        dm_tor   = DM_motor->motor_msg.torque_current;
         // 控制电机的位置和速度
-        MIT_CtrlMotorDM(DM_motor, dm_pos, 1, 1.0f, 1.0f, 0.0f);
+        MIT_CtrlMotorDM(DM_motor, dm_pos, 0, 1.0f, 0.0f, 0.0f);
+
+        osDelay(1);
     }   
 }
