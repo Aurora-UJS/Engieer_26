4a2186711c60da4c1aede3f059b394bd4c27cc79
huangJiazhi <huangjia2hi@163.com>
2026-01-04 07:39:50 +0800

    大臂代码 六关节控制，接收自定义控制器的回传角度

--- source/task/joint.c
+++ source/task/joint.c
@@ -4,27 +4,36 @@
 #include "uart_api.h"
 #include "motor_DM.h"
 #include "cmsis_os2.h"
+#include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 #include "arm_math.h"
+#include "tool.h"
 #define JOINT_NUM 6
 
 DM_motor_t *joint_motor[JOINT_NUM];
-uart_rx_t Angle_msg;
+uart_rx_t Angle_msg; // 
 uart_msg_t Angle_rx_msg;
-uart_msg_t Angle_tx_msg;
+// uart_msg_t Angle_tx_msg; 测试
 uint8_t Angle_rx_msg_Buffer[256];
+// uint8_t testBuf[50] = {0};
 float joint_radian[6] = {0};
-
+uint8_t firstEnableFlag = 0;
 void Angle_Receive_Callback(uint8_t *buf, uint32_t len)
 {
-    // HAL_UART_Transmit(&huart7, buf, len, 100);  // 回传显示
-
-    for (int i = 0; i < 6; i++) {
-        int tmp = 0;
-        // 每个弧度占 4 个字符
-        sscanf((const char*)&buf[i * 4], "%04d", &tmp);
-        joint_radian[i] = tmp / 100.0f;
-        joint_radian[i] = joint_radian[i] - PI;
+    // HAL_UART_Transmit(&huart7, buf, len, 100);  // 回传显示 测试
+    // memcpy(testBuf, buf, sizeof(buf)); 测试
+    if (firstEnableFlag == 0) {
+        firstEnableFlag = 1;
+    }
+    else {
+        for (int i = 0; i < 6; i++) {
+            int tmp = 0;
+            // 每个弧度占 4 个字符
+            sscanf((const char*)&buf[i * 4], "%04d", &tmp);
+            joint_radian[i] = tmp / 1000.0f;
+            joint_radian[i] = joint_radian[i] - PI;
+        }
     }
 }
 void angle_msg_rx_init(void)
@@ -36,12 +45,12 @@ void angle_msg_rx_init(void)
     uart7_rx_hook = Angle_Receive_Callback;
     uart_rx_init(&Angle_msg);
 }
-uart_msg_t testUart_tx_msg;
-void testUart_tx_init(void){
-    testUart_tx_msg.huart= &huart7;
-    testUart_tx_msg.pBuffer = (uint8_t *)"hello\r\n";
-    testUart_tx_msg.Len = strlen((char *) testUart_tx_msg.pBuffer);
-}
+// uart_msg_t testUart_tx_msg;
+// void testUart_tx_init(void){
+//     testUart_tx_msg.huart= &huart7;
+//     testUart_tx_msg.pBuffer = (uint8_t *)"hello\r\n";
+//     testUart_tx_msg.Len = strlen((char *) testUart_tx_msg.pBuffer);
+// }
 
 void uart_Transmit_Angle(void *argment)
 {
@@ -69,27 +78,34 @@ void joint_motor_init(void)
         Motor_DM_Init(joint_motor[joint_index]);
     }
 }
+void Joint_Motor_Refresh(void)
+{
+    for (int joint_index = 0; joint_index < JOINT_NUM; joint_index++) {
+        Motor_DM_Refresh(joint_motor[joint_index]);
+    }
+}
+void Joint_Motor_Enable(void)
+{
+    for (int joint_index = 0; joint_index < JOINT_NUM; joint_index++) {
+        Motor_DM_Enable(joint_motor[joint_index]);
+    }
+}
 float dm_angle_test = 0;
 void jointFollowAngle(void *argument)
 {
     UNUSED(argument);
     joint_motor_init();
-    Motor_DM_Enable(joint_motor[0]);
+    Joint_Motor_Enable(); // 使能所有电机
     while (1) {
-        dm_angle_test = joint_motor[0]->motor_msg.motor_angle;
-        Motor_DM_Refresh(joint_motor[0]);
-        // Motor_DM_Refresh(joint_motor[1]);
-        // Motor_DM_Refresh(joint_motor[2]);
-        // Motor_DM_Refresh(joint_motor[3]);
-        // Motor_DM_Refresh(joint_motor[4]);
-        // Motor_DM_Refresh(joint_motor[5]);
+        // dm_angle_test = joint_motor[3]->motor_msg.motor_angle; 测试
+        Joint_Motor_Refresh();
 
-        MIT_CtrlMotorDM(joint_motor[0],joint_radian[4], 0, 1, 0, 0);
-        // MIT_CtrlMotorDM(joint_motor[1],joint_radian[1], 0, 1, 0, 0);
-        // MIT_CtrlMotorDM(joint_motor[2],joint_radian[2], 0, 1, 0, 0);
-        // MIT_CtrlMotorDM(joint_motor[3],joint_radian[3], 0, 1, 0, 0);
-        // MIT_CtrlMotorDM(joint_motor[4],joint_radian[4], 0, 1, 0, 0);
-        // MIT_CtrlMotorDM(joint_motor[5],joint_radian[5], 0, 1, 0, 0);
-        osDelay(2);
+        PosSpeed_CtrlMotorDM(joint_motor[0],joint_radian[0], 1);
+        PosSpeed_CtrlMotorDM(joint_motor[1],joint_radian[1], 1);
+        PosSpeed_CtrlMotorDM(joint_motor[2],joint_radian[2], 1);
+        PosSpeed_CtrlMotorDM(joint_motor[3],joint_radian[0], 1);
+        PosSpeed_CtrlMotorDM(joint_motor[4],joint_radian[4], 1);
+        PosSpeed_CtrlMotorDM(joint_motor[5],joint_radian[5], 1);
+        osDelay(1);
     }
 }
