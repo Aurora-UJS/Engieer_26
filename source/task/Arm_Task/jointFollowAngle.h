f94c0ea3be35e089b56111d6dd72b75e8821b983
huangJiazhi <huangjia2hi@163.com>
2026-01-20 10:54:16 +0800

    重构joint.c的代码 将原来的joint.c中的 jointFollowAngle 任务函数移至jointFollowAngle.c 并添加 jointFollowAngle.h 来管理定义

---
+++ source/task/Arm_Task/jointFollowAngle.h
@@ -0,0 +1,12 @@
+#ifndef JOINTFOLLOWANGLE
+#define JOINTFOLLOWANGLE
+    
+#include "motor_DM.h"
+#include "cmsis_os2.h"
+#include "tool.h"
+#include "arm_math.h"
+#include <stdio.h>
+#include "DBusSys.h"
+#define JOINT_NUM 7
+
+#endif
