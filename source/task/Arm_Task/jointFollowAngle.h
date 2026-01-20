6f5284e1d89b293871128827200405c3abf5eccd
huangJiazhi <huangjia2hi@163.com>
2026-01-20 10:59:35 +0800

    完善jointFollowAngle.h

--- source/task/Arm_Task/jointFollowAngle.h
+++ source/task/Arm_Task/jointFollowAngle.h
@@ -9,4 +9,12 @@
 #include "DBusSys.h"
 #define JOINT_NUM 7
 
+/** @brief 电机初始化*/
+void joint_motor_init(void);
+/** @brief 电机信息更新 */
+void Joint_Motor_Refresh(void);
+/** @brief 电机使能 */
+void Joint_Motor_Enable(void);
+/** @brief 任务jointFollowAngle */
+void jointFollowAngle(void *argument);
 #endif
