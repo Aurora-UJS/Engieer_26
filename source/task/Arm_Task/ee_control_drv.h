0e8fc1ca0e41c735ef1bbb4a6e702c0efa8804ef
huangJiazhi <huangjia2hi@163.com>
2026-01-22 02:44:32 +0800

    添加夹爪状态定义

--- source/task/Arm_Task/ee_control_drv.h
+++ source/task/Arm_Task/ee_control_drv.h
@@ -7,6 +7,11 @@ typedef struct endEffector_t{
     DM_motor_t *endEffector_motor;
 } endEffector_t;
 
+typedef enum{
+    GPIPER_STATE_IDLE = 0,
+    GRIPPER_STATE_OPEN,
+    GPIPER_STATE_CLOSE
+}GPIPER_STATE ;
 /** @brief 末端执行器初始化*/
 void endeffector_init(endEffector_t *endeffector);
 
@@ -18,4 +23,5 @@ void EndEffector_Motor_Refresh(endEffector_t *endeffector);
 
 /** @brief 末端执行器电机使能 */
 void EndEffector_Motor_Enable(endEffector_t *endeffector);
+
 #endif
