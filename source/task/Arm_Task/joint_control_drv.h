f7b6ea3057108798c278b7e3f2a1ba55c2590812
huangJiazhi <huangjia2hi@163.com>
2026-01-29 04:55:36 +0800

    can3修改

--- source/task/Arm_Task/joint_control_drv.h
+++ source/task/Arm_Task/joint_control_drv.h
@@ -34,7 +34,7 @@ static const float joint_mannal_polarity_map[JOINT_NUM] = {
     PROSITIVE, PROSITIVE, PROSITIVE, PROSITIVE, PROSITIVE, PROSITIVE};
 
 static const can_port_t can_port_map[JOINT_NUM] = {
-    CAN3_PORT, CAN2_PORT, CAN2_PORT, CAN2_PORT, CAN2_PORT, CAN2_PORT,
+    CAN3_PORT, CAN3_PORT, CAN2_PORT, CAN2_PORT, CAN2_PORT, CAN2_PORT,
 };
 static const joint_dof_t joint_dof_map[JOINT_NUM] = {
     JOINT_DOF_YAW,  JOINT_DOF_PITCH, JOINT_DOF_PITCH,
