ebbe8f9a4278a67eb9b21608dd689843fc066582
huangjia2hi <huangjia2hi@163.com>
2026-02-25 15:11:32 +0800

    将api标注为弃用

--- source/task/Joint_Move/Joint_Move_Task.c
+++ source/task/Joint_Move/Joint_Move_Task.c
@@ -6,7 +6,7 @@
 
 extern target_point_t Target_Point[6];
 extern Joint_t Joint[JOINT_NUM];
-
+__attribute__((deprecated))
 void Joint1_Move_Task(void *argument) {
   Joint[0].joint_motor = pvPortMalloc(sizeof(DM_motor_t));
   Joint[0].joint_motor->can_cfg.id = 0x01;
@@ -23,13 +23,38 @@ void Joint1_Move_Task(void *argument) {
   Motor_DM_Enable(Joint[0].joint_motor);
   UNUSED(argument);
   while (1) {
-    PosSpeed_CtrlMotorDM(Joint[0].joint_motor,
-                         Target_Point[0].target_joint_radian,
-                         Target_Point[0].velocity);
-    osDelay(1);
+    /* for (int joint_index = 0; joint_index < JOINT_NUM; joint_index++) {
+      PosSpeed_CtrlMotorDM(Joint[joint_index].joint_motor,
+                           Target_Point[joint_index].target_joint_radian,
+                           Target_Point[joint_index].velocity);
+      osDelay(2);
+    } */
+
+
+/*     Joint_Motor_PosSpeed_Ctrl(&Joint[1],Target_Point[1]);
+    osDelay(2);
+
+    Joint_Motor_PosSpeed_Ctrl(&Joint[4],Target_Point[4]);
+    osDelay(2);
+
+
+    Joint_Motor_PosSpeed_Ctrl(&Joint[2],Target_Point[2]);
+    osDelay(2);
+
+
+
+    Joint_Motor_PosSpeed_Ctrl(&Joint[0],Target_Point[0]);
+    osDelay(2);
+
+    Joint_Motor_PosSpeed_Ctrl(&Joint[3],Target_Point[3]);
+    osDelay(2);
+
+    Joint_Motor_PosSpeed_Ctrl(&Joint[5],Target_Point[5]);
+    osDelay(2); */
   }
 }
 
+__attribute__((deprecated))
 void Joint2_Move_Task(void *argument) {
   Joint[1].joint_motor = pvPortMalloc(sizeof(DM_motor_t));
   Joint[1].joint_motor->can_cfg.id = 0x02;
@@ -47,13 +72,14 @@ void Joint2_Move_Task(void *argument) {
   UNUSED(argument);
 
   while (1) {
-    PosSpeed_CtrlMotorDM(Joint[1].joint_motor,
-                         Target_Point[1].target_joint_radian,
-                         Target_Point[1].velocity);
-    osDelay(1);
+    // PosSpeed_CtrlMotorDM(Joint[1].joint_motor,
+    //                      Target_Point[1].target_joint_radian,
+    //                      Target_Point[1].velocity);
+    osDelay(3);
   }
 }
 
+__attribute__((deprecated))
 void Joint3_Move_Task(void *argument) {
   Joint[2].joint_motor = pvPortMalloc(sizeof(DM_motor_t));
   Joint[2].joint_motor->can_cfg.id = 0x03;
@@ -71,13 +97,14 @@ void Joint3_Move_Task(void *argument) {
   UNUSED(argument);
 
   while (1) {
-    PosSpeed_CtrlMotorDM(Joint[2].joint_motor,
-                         Target_Point[2].target_joint_radian,
-                         Target_Point[2].velocity);
-    osDelay(1);
+    // PosSpeed_CtrlMotorDM(Joint[2].joint_motor,
+    //                      Target_Point[2].target_joint_radian,
+    //                      Target_Point[2].velocity);
+    osDelay(3);
   }
 }
 
+__attribute__((deprecated))
 void Joint4_Move_Task(void *argument) {
   Joint[3].joint_motor = pvPortMalloc(sizeof(DM_motor_t));
   Joint[3].joint_motor->can_cfg.id = 0x04;
@@ -95,13 +122,14 @@ void Joint4_Move_Task(void *argument) {
   UNUSED(argument);
 
   while (1) {
-    PosSpeed_CtrlMotorDM(Joint[3].joint_motor,
-                         Target_Point[3].target_joint_radian,
-                         Target_Point[3].velocity);
-    osDelay(1);
+    // PosSpeed_CtrlMotorDM(Joint[3].joint_motor,
+    //                      Target_Point[3].target_joint_radian,
+    //                      Target_Point[3].velocity);
+    osDelay(3);
   }
 }
 
+__attribute__((deprecated))
 void Joint5_Move_Task(void *argument) {
   Joint[4].joint_motor = pvPortMalloc(sizeof(DM_motor_t));
   Joint[4].joint_motor->can_cfg.id = 0x05;
@@ -119,13 +147,14 @@ void Joint5_Move_Task(void *argument) {
   UNUSED(argument);
 
   while (1) {
-    PosSpeed_CtrlMotorDM(Joint[4].joint_motor,
-                         Target_Point[4].target_joint_radian,
-                         Target_Point[4].velocity);
-    osDelay(1);
+    // PosSpeed_CtrlMotorDM(Joint[4].joint_motor,
+    //                      Target_Point[4].target_joint_radian,
+    //                      Target_Point[4].velocity);
+    osDelay(3);
   }
 }
 
+__attribute__((deprecated))
 void Joint6_Move_Task(void *argument) {
   Joint[5].joint_motor = pvPortMalloc(sizeof(DM_motor_t));
   Joint[5].joint_motor->can_cfg.id = 0x06;
@@ -143,9 +172,9 @@ void Joint6_Move_Task(void *argument) {
   UNUSED(argument);
 
   while (1) {
-    PosSpeed_CtrlMotorDM(Joint[5].joint_motor,
-                         Target_Point[5].target_joint_radian,
-                         Target_Point[5].velocity);
-    osDelay(1);
+    // PosSpeed_CtrlMotorDM(Joint[5].joint_motor,
+    //                      Target_Point[5].target_joint_radian,
+    //                      Target_Point[5].velocity);
+    osDelay(3);
   }
 }
