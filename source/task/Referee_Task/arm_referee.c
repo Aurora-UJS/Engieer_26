925794ccde8f0cdbaa90bb076530c3fe7ec58d0e
huangjia2hi <huangjia2hi@163.com>
2026-02-27 20:54:43 +0800

    添加四个命令

--- source/task/Referee_Task/arm_referee.c
+++ source/task/Referee_Task/arm_referee.c
@@ -1,12 +1,26 @@
 #include "arm_state_machine.h"
 #include "trajectory_publisher_drv.h"
+#include <stdint.h>
 
-void Arm_Keyboard_Manager(uint8_t key){
+void Arm_Keyboard_Manager(uint8_t key) {
 
-    if (key == (uint8_t)'Q') {
-        Arm_Current_Control_Mode = Arm_Traj_Mode;
-        cmd_place_get = Command_getRight; 
-    }
-    
-    
+  if (key == (uint8_t)'Q') {
+    Arm_Current_Control_Mode = Arm_Traj_Mode;
+    cmd_place_get = Command_getLeft;
+  }
+
+  if (key == (uint8_t)'E') {
+    Arm_Current_Control_Mode = Arm_Traj_Mode;
+    cmd_place_get = Command_getRight;
+  }
+
+  if (key == (uint8_t)'R') {
+    Arm_Current_Control_Mode = Arm_Traj_Mode;
+    cmd_place_get = Command_placeLeft;
+  }
+
+  if (key == (uint8_t)'T') {
+    Arm_Current_Control_Mode = Arm_Traj_Mode;
+    cmd_place_get = Command_placeRight;
+  }
 }
