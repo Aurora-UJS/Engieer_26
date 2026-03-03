0d4c6daa5ae5c2fa62c2015051f8af6e42dcabb0
huangjia2hi <huangjia2hi@163.com>
2026-03-03 22:30:23 +0800

    使用ai 的api暂没有经过测试

--- source/module/refereeSys/referee_api.c
+++ source/module/refereeSys/referee_api.c
@@ -275,7 +275,7 @@ static void ctrller_parse_frame(uint8_t *buff, uint16_t len)
 {
 	// 调试用：复制帧数据
 	memcpy(Ctrller_Receive_Buffer, buff, len < 256 ? len : 256);
-	memcpy(CtrllerData, Ctrller_Receive_Buffer + 7, 26);
+	// memcpy(CtrllerData, Ctrller_Receive_Buffer + 7, 26);
 	// 写入帧头数据
 	memcpy(&custom_controller_info.FrameHeader, buff, LEN_HEADER);
 	
@@ -287,6 +287,7 @@ static void ctrller_parse_frame(uint8_t *buff, uint16_t len)
 	{
 	case 0x0302:  // 自定义控制器数据
 		memcpy(&custom_controller_info.CustomController, (buff + DATA_Offset), LEN_custom_controller);
+		memcpy(CtrllerData, &custom_controller_info.CustomController, LEN_custom_controller);
 		break;
 	case 0x0304:  // 键鼠数据
 		memcpy(&custom_controller_info.keyboard, (buff + DATA_Offset), LEN_keyboard);
