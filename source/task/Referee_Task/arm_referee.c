116b86a837fd0501d58151483331b053563e9c2d
huangjia2hi <huangjia2hi@163.com>
2026-03-25 11:56:43 +0800

    test:按键测试两个取矿轨迹

--- source/task/Referee_Task/arm_referee.c
+++ source/task/Referee_Task/arm_referee.c
@@ -7,16 +7,14 @@
 extern osTimerId_t traj_timer_id;
 extern rc_info_t remoter;
 static Command_Place_And_Get_t get_command_table[] ={
-  Command_getBackRight,
   Command_getRight,
   Command_getLeft
 };
 static Command_Place_And_Get_t place_command_table[] = {
   Command_placeLeft,
   Command_placeRight,
-  Command_placeBackRight
 };
-#define COMMAND_TABLE_SIZE 3
+#define COMMAND_TABLE_SIZE 2
 static int place_key_command_index = 0;
 static int get_key_command_index = 0;
 
@@ -36,7 +34,7 @@ void Arm_Keyboard_Manager(uint8_t key) {
     cmd_place_get = place_command_table[place_key_command_index];
     traj_point_index = 0;
     osTimerStop(traj_timer_id);
-    osTimerStart(traj_timer_id, 5); // <-- 确保定时器启动
+    osTimerStart(traj_timer_id, 5); 
     }
   if (key == (uint8_t)'F') {
     Arm_Current_Control_Mode = Arm_Custom_Controller_Follow_Mode;
