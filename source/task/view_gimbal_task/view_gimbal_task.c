a7549f5a5e38b0cc12384db9e106fe9456ce9809
huangjia2hi <huangjia2hi@163.com>
2026-04-05 19:21:33 +0800

    舵机驱动已经添加相关文档说明

--- source/task/view_gimbal_task/view_gimbal_task.c
+++ source/task/view_gimbal_task/view_gimbal_task.c
@@ -3,11 +3,11 @@
 
 void View_Gimbal_Task(void *argument){
   UNUSED(argument);
-  __HAL_TIM_MOE_ENABLE(&htim1); 
-  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_3);
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+  int ecd = 1500;
   while(1)
   {
-    __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_3, 1500);
-    osDelay(2);
+    __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, ecd);
+    osDelay(10);
   }
 }
