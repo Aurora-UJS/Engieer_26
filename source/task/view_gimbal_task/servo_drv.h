9aa86a34e2a9dcfc6c66adeecfcddffc244f4f42
huangjia2hi <huangjia2hi@163.com>
2026-04-06 15:03:36 +0800

    封装舵机驱动

---
+++ source/task/view_gimbal_task/servo_drv.h
@@ -0,0 +1,12 @@
+#ifndef SERVO_DRV_H
+#include "tim.h"
+#include "stm32h7xx_hal_tim.h"
+typedef struct {
+  TIM_HandleTypeDef *htim; 
+  uint32_t Channel;
+  int16_t position; 
+} servo_t;
+void servo_init(servo_t *servo,TIM_HandleTypeDef *htim, uint32_t Channel);
+void servo_drive(servo_t* servo);
+void servo_setPos(servo_t* servo,float position);
+#endif // !SERVO_DRV_H
